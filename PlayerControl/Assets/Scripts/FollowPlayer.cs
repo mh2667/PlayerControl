d0cca3782732c3aa761a6bd7edccca6d808e8e14
Maxwell Hernandez <hernandezm3@roverkids.org>
2024-04-11 13:29:21 -0400

    Completed 1.3 Step 5

--- PlayerControl/Assets/Scripts/FollowPlayer.cs
+++ PlayerControl/Assets/Scripts/FollowPlayer.cs
@@ -14,7 +14,7 @@ public class FollowPlayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         // Offset the camera behind the player by adding to the player's position
         transform.position = player.transform.position + offset;
