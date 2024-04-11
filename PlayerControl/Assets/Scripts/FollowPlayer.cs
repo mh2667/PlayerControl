a011d667c18c86f62c31b4448d8ae88262659b1e
Maxwell Hernandez <hernandezm3@roverkids.org>
2024-04-11 13:21:35 -0400

    Completed 1.3 Step 2

--- PlayerControl/Assets/Scripts/FollowPlayer.cs
+++ PlayerControl/Assets/Scripts/FollowPlayer.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = player.transform.position;
     }
 }
